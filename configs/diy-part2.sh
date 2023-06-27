c3cdddc0b9b91f16fb237dc36b14ad10ec93b375
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-06-27 23:55:32 +0800

    Update diy-part2.sh

--- configs/diy-part2.sh
+++ configs/diy-part2.sh
@@ -4,6 +4,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# Set DISTRIB_REVISION
+sed -i "s/OpenWrt /TIAmo Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
 # 移除重复软件包
 rm -rf feeds/luci/themes/luci-theme-argon
 
